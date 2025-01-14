9bef1964f7f2f97240adb4cce49b944c97a46a24
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-14 18:20:58 +0300

    14.01.2025

---
+++ BookCaveProject/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCaveProject.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error1()
+        {
+            return View();
+        }
+    }
+}
