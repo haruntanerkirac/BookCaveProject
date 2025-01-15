6ed9133a5d7e6df459741268810aba4ff57cecc7
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-15 19:08:12 +0300

    15.01.2025

---
+++ BookCaveProject/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCaveProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = aboutManager.GetAll();
+            return View(values);
+        }
+    }
+}
